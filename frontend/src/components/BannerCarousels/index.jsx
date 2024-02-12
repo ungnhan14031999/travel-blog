45594336c6b6a7513222be144dda3ddeded4469c
ungnhan14031999 <ungnhan14031999@gmail.com>
2024-02-12 21:46:07 +0700

    fix component carousel banner

--- frontend/src/components/BannerCarousels/index.jsx
+++ frontend/src/components/BannerCarousels/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import Slider from "react-slick";
 
 import BannerImg from '../../assets/image/travel_blog_2.jpg'
 
 function BannerCarousels() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+    };
+
     return (
-        <></>
+        <header>
+            <Slider {...settings}>
+                <div>
+                    <img src={BannerImg} />
+                </div>
+                <div>
+                    <img src={BannerImg} />
+                </div>
+                <div>
+                    <img src={BannerImg} />
+                </div>
+            </Slider>
+        </header>
     );
 }
 
